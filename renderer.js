c3a7e73b60fe52afbc3dd2383311dbc6c021cedf
Mohamed Ahmed <error17191@gmail.com>
2018-10-07 14:09:05 +0200

    Integrating Vuetify

--- renderer.js
+++ renderer.js
@@ -1,3 +1,26 @@
-document.querySelector('#check').addEventListener('click',() => {
-    alert('working');
+// const {machineIdSync} = require('node-machine-id');
+// const machineId = machineIdSync();
+// const qrcode = require('qrcode-generator');
+//
+// var typeNumber = 4;
+// var errorCorrectionLevel = 'L';
+// var qr = qrcode(typeNumber, errorCorrectionLevel);
+// qr.addData('Hi!');
+// qr.make();
+// document.getElementById('placeHolder').innerHTML = qr.createImgTag();
+//
+// document.querySelector('#check').addEventListener('click',() => {
+//     axios.post(url('/request_work_machine'), {machine_id : machineId })
+//         .then(response => {
+//
+//         });
+// });
+
+
+document.getElementById('signin').addEventListener('click', () => {
+    axios.post(url('api/login'), {
+
+    }).then(response => {
+            console.log(response.data);
+        })
 });
---
+++ src/js/app.js
@@ -0,0 +1,20 @@
+import 'material-design-icons-iconfont/dist/material-design-icons.css' // Ensure you are using css-loader
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
+
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+
+Vue.use(Vuetify)
+import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
+
+
+new Vue({
+    el: '#app',
+    data: () => ({
+        drawer: null
+    }),
+
+    props: {
+        source: String
+    }
+})
